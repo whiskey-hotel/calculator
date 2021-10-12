6c06da35c3a2ad0bf9e183548ecfcf9a017cb203
Justin Thornton <jathom15@gmail.com>
2021-10-11 20:51:18 -0500

    first commit

---
+++ scripts/logic.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+function operate(operator, a, b) {
+	switch (operator) {
+		case '+':
+			add(a, b);
+			break;
+		case '-':
+			subtract(a, b);
+			break;
+		case '*':
+			multiply(a, b);
+			break;
+		case '/':
+			divide(a, b);
+			break;
+        case "=":
+            return
+	}
+	return;
+}
