a96f60e830a38e92c8e71c75a8ac374fe7b0f048
Justin Thornton <jathom15@gmail.com>
2021-10-14 00:34:23 -0500

    Styling and event listeners for numbers and operators

--- scripts/logic.js
+++ scripts/logic.js
@@ -33,3 +33,34 @@ function operate(operator, a, b) {
 	}
 	return;
 }
+
+const clear = document.querySelector('#clear');
+const result = document.querySelector('#span-result');
+const num = document.getElementsByClassName('num');
+const DOMoperator = document.getElementsByClassName('operator');
+let num1;
+let num2;
+let operator = null;
+
+for (let n of Array.from(num)) {
+	n.addEventListener('click', function () {
+		let value = n.dataset.value;
+		let text = document.createTextNode(value);
+		result.appendChild(text);
+		a = result.textContent;
+	});
+}
+
+for (let op of Array.from(DOMoperator)) {
+	op.addEventListener('click', function () {
+		operator = op.dataset.value;
+	});
+}
+
+clear.addEventListener('click', function () {
+	result.textContent = '0';
+	operator = null;
+});
+
+if (!!operator) {
+}
